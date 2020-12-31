4af86b94d365942004ef23533d3339bf30afc5b4
netdesignr <michaiel.webdesigner@gmail.com>
2020-12-31 14:57:18 +0000

    Added additional content to cookie banner

--- v1/The National Archives_files/tna-child-home.js
+++ v1/The National Archives_files/tna-child-home.js
@@ -2,23 +2,21 @@
  * TNA Child home JS
  */
 
-$(document).ready(function() {
+$(document).ready(function () {
+  var $count = jQuery(".col-card").length;
 
-    var $count = jQuery(".col-card").length;
-
-    if ($count < 3) {
-        var elements = document.getElementsByClassName("col-card");
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].hide();
-        }
+  if ($count < 3) {
+    var elements = document.getElementsByClassName("col-card");
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].hide();
     }
+  }
 
-    if ( $count > 3 && $count < 6 ) {
-        var elements = document.getElementsByClassName("col-card");
+  if ($count > 3 && $count < 6) {
+    var elements = document.getElementsByClassName("col-card");
 
-        for (var i = 3; i < elements.length; i++) {
-
-            elements[i].hide();
-        }
+    for (var i = 3; i < elements.length; i++) {
+      elements[i].hide();
     }
+  }
 });
