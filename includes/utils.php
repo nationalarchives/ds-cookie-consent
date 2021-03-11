dd3b4fa8417683bd5b9acc880511c2a3a9e2a82a
netdesignr <michaiel.webdesigner@gmail.com>
2021-03-11 12:36:23 +0000

    Update prompt message on cookie settings

--- includes/utils.php
+++ includes/utils.php
@@ -12,7 +12,7 @@ function decode_cookie($cookie_name) {
 
 // Success message on the Cookie Settings Page
 function cookie_success_message() {
-    return '<div class="emphasis-block success-message" role="alert"><p class="h3">Your cookie settings were saved</p><p>Thank you!</p></div>';
+    return '<div class="emphasis-block success-message" role="alert"><p class="h3">Thank you</p><p>Your cookies settings have been saved.</p></div>';
 }
 
 // Create the HTML Cookie Banner
