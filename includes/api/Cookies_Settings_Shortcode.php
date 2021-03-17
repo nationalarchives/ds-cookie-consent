354970de71138e42a89d17258314e3855359746a
AshTNA <ashleyaburnett@gmail.com>
2021-03-17 12:16:46 +0000

    Fix wrapping on cookie page and keyboard focus styles

--- includes/api/Cookies_Settings_Shortcode.php
+++ includes/api/Cookies_Settings_Shortcode.php
@@ -54,10 +54,14 @@ function shortcode_settings_page( $atts ) {
     $output .= '<h3>' . get_option('our_first_field_headline') . '</h3>';
     $output .= get_option('our_first_field');
     $output .= '<br><br>';
+    $output .= '<div class="form-group">';
     $output .= '<input type="radio" id="measure_website_use" name="measure-website-use" value="yes" ' . $accept_usage_option . '>';
     $output .= '<label for="measure_website_use">Use cookies that measure my website use</label><br>';
+    $output .= '</div>';
+    $output .= '<div class="form-group">';
     $output .= '<input type="radio" id="donot_measure_website_use" name="measure-website-use" value="no" ' . $reject_usage_option . '>';
     $output .= '<label for="donot_measure_website_use">Do not use cookies that measure my website use</label><br>';
+    $output .= '</div>';
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ? '<h2>' . get_option('our_second_field_headline') . '</h2>' : '';
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ? get_option('our_second_field') : '';
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ?'<br><br>' : '';
