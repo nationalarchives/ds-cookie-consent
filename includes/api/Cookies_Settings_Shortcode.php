7f32e7ffb70770f33ff99e5007c6ad8f136494bc
AshTNA <ashleyaburnett@gmail.com>
2022-05-12 15:42:54 +0100

    Reduce whitespace on form
    As requested by editors in FE review

--- includes/api/Cookies_Settings_Shortcode.php
+++ includes/api/Cookies_Settings_Shortcode.php
@@ -24,8 +24,7 @@ function shortcode_settings_page( $atts ) {
     $output .= (!empty($_POST)) ? cookie_success_message() : '';
     $output .= '<legend class="sr-only">Cookie settings</legend>';
     $output .= '<h2>' . get_option('our_first_field_headline') . '</h2>';
-    $output .= get_option('our_first_field');
-    $output .= '<br><br>';
+    $output .= '<p class="reduced-margin">' . get_option('our_first_field') . '</p>';
     $output .= '<div class="form-group">';
     $output .= '<input type="radio" id="measure_website_use" name="measure-website-use" value="yes">';
     $output .= '<label for="measure_website_use">Use cookies that measure my website use</label><br>';
@@ -35,8 +34,7 @@ function shortcode_settings_page( $atts ) {
     $output .= '<label for="donot_measure_website_use">Do not use cookies that measure my website use</label><br>';
     $output .= '</div>';
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ? '<h2>' . get_option('our_second_field_headline') . '</h2>' : '';
-    $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ? get_option('our_second_field') : '';
-    $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ? '<br><br>' : '';
+    $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ? '<p class="reduced-margin">' . get_option('our_second_field') . '</p>' : '';
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ?'<div class="form-group">': '';
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ?'<input type="radio" id="remember_your_settings" name="remember-your-settings" value="yes">' : '';
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ?'<label for="remember_your_settings">Use cookies that remember my preferences</label><br>' : '';
@@ -46,8 +44,7 @@ function shortcode_settings_page( $atts ) {
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ?'<label for="donot_remember_your_settings">Do not use cookies that remember my preferences</label><br>' : '';
     $output .= (isset($atts['settings']) && $atts['settings'] == 'show') ?'</div>': '';
     $output .= '<h2>' . get_option('our_third_field_headline') . '</h2>';
-    $output .= get_option('our_third_field');
-    $output .= '<br><br>';
+    $output .= '<p class="reduced-margin">' . get_option('our_third_field') . '</p>';
     $output .= '<div class="tna-form__row"><input type="submit" name="submit" id="form_submit" value="Save cookies" class="tna-button"></div>';
     $output .= '</fieldset>';
     $output .= '</form>';
